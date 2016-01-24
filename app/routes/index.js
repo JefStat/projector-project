6703f1707b4e9be566be35fef9f74c226bfee6e8
Jef Statham <jef.statham@gmail.com>
2016-01-24 16:51:34 -0500

    Display images in the carousel

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
 var router = express.Router();
+var imageDir = __dirname + '/../public/photos/';
+var publicPhotoDir = 'photos/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  getImages(imageDir, function (err, files) {
+    if (err) { console.error(err); res.status(500).send(err); return; }
+    res.render('index', { title: 'Photo Project', images: files });
+  });
 });
+
+function getImages(imageDir, cb) {
+  var fileType = '.jpg';
+  var files = [];
+  fs.readdir(imageDir, function (err, list) {
+    if (err) { cb(err); return; }
+    for(var i=0; i<list.length; i++) {
+      if(path.extname(list[i]) === fileType) {
+        files.push(publicPhotoDir + list[i]); //store the file name into the array files
+      }
+    }
+    cb(err, files);
+  });
+}
 
 module.exports = router;
