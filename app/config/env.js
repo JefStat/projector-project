5824e352247b3bc9200eba7b34f9c042302d13d4
Jef Statham <jef.statham@gmail.com>
2016-01-26 11:06:23 -0500

    Add skeleton env.js

---
+++ app/config/env.js
@@ -0,0 +1,7 @@
+
+process.env.TWITTER_CONSUMER_KEY = '';
+process.env.TWITTER_CONSUMER_SECRET = '';
+process.env.TWITTER_ACCESS_TOKEN_KEY = '';
+process.env.TWITTER_ACCESS_TOKEN_SECRET = '';
+
+module.exports = {};
