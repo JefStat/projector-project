6177baf7acf41b2d8cd14773ddce4d2a64e5648b
Jef Statham <jef.statham@gmail.com>
2016-01-28 11:34:59 -0500

    Do not disconnect call backs when multiple clients connect

--- app/bin/twitter-stream.js
+++ app/bin/twitter-stream.js
@@ -1,12 +1,13 @@
 'use strict';
 var _ = require('lodash');
+var env = require('../config/env.js');
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  consumer_key: env.TWITTER_CONSUMER_KEY,
+  consumer_secret: env.TWITTER_CONSUMER_SECRET,
+  access_token_key: env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 var isPhotoBomb = function(tweet) {
