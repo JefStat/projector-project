6431c9ec94c750dfb0eddd4f6e599f7a7e8954d9
Jef Statham <jef.statham@gmail.com>
2016-01-28 12:01:42 -0500

    Use imgix urls for photos

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,22 +5,43 @@ var env = require('../config/env.js');
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
+var _ = require('lodash');
 var router = express.Router();
 
-
-var client = new ImgixClient(env.IMGIX_DOMAIN, env.IMGIX_SECURE_TOKEN);
+var imgixClient = new ImgixClient(env.IMGIX_DOMAIN, env.IMGIX_SECURE_TOKEN);
+var fileNameList = path.join(__dirname, '../public/photos.txt');
 
 var mkdirp = require('mkdirp');
-var imageDir = __dirname + '/../public/photos/';
+var imageDir = path.join(__dirname, '../public/photos/');
 var publicPhotoDir = 'photos/';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  getImages(imageDir, function (err, files) {
+  getImgix(fileNameList, function (err, files) {
     if (err) { console.error(err); res.status(500).send(err); return; }
     res.render('index', { title: 'Photo Project', images: files });
   });
+
+  // getImages(imageDir, function (err, files) {
+  //   if (err) { console.error(err); res.status(500).send(err); return; }
+  //   res.render('index', { title: 'Photo Project', images: files });
+  // });
 });
+
+function getImgix(fileNameList, cb) {
+  fileNameList
+  fs.readFile(fileNameList, function read(err, data) {
+    if (err) {
+      cb(err, null);
+    }
+    var list = _.split(data, '\n');
+    var fileNameArray = [];
+    for(var i=0; i<list.length; i++)  {
+      fileNameArray.push(imgixClient.path(list[i]).toUrl({ w: 1024, h: 768 }).toString());
+    }
+    cb(err, fileNameArray);
+  });
+}
 
 function getImages(imageDir, cb) {
   var fileType = '.jpg';
@@ -32,7 +53,8 @@ function getImages(imageDir, cb) {
       if (err) { cb(err); return; }
       for(var i=0; i<list.length; i++) {
         if(path.extname(list[i]) === fileType) {
-          files.push(publicPhotoDir + list[i]); //store the file name into the array files
+          //store the file name into the array files
+          files.push(path.join(publicPhotoDir, list[i])); 
         }
       }
       cb(err, files);
